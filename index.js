ef669b2303ac75bf62cc75631ae808fbe6951ca4
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-01-06 10:46:10 +0100

    Moved event handling into separate files

--- index.js
+++ index.js
@@ -7,38 +7,24 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Load all commands
 client.commands = new Collection();
-const commandFiles = fs
-  .readdirSync('./commands')
-  .filter((file) => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-  console.log('Competition bot is up and running.');
-});
+// Load all events
+const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'));
 
-// Listen to commands
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      ephemeral: true,
-    });
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 // Login to Discord with your client's token
 client.login(token);
