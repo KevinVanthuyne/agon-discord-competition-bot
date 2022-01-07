3bc037d571bbbf056363d5a765cece9527e7ceac
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-01-07 12:56:54 +0100

    Reply only to !scored

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,8 @@
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
+    if (!message.content.startsWith('!scored ')) return;
     console.log(message);
+    message.reply(`<@${message.author.id}> posted a new score!`);
   },
 };
