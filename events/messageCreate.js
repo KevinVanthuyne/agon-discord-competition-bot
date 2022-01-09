f9d3efacfe30f8034305354436acfd737c6463d4
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-01-09 09:41:53 +0100

    Trying to attach an attachment to the bot's reply

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,22 @@
+const { MessagePayload } = require('discord.js');
+
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
     if (!message.content.startsWith('!scored ')) return;
+
     console.log(message);
-    message.reply(`<@${message.author.id}> posted a new score!`);
+
+    new MessagePayload(message, {
+      content: `<@${message.author.id}> posted a new score!`,
+      files: [
+        {
+          buffer: message.attachment,
+        },
+      ],
+    });
+    MessagePayload.create();
+
+    await message.reply(`<@${message.author.id}> posted a new score!`);
   },
 };
