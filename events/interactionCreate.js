9abd6e51febfb15064f75e20630db9f25dd96c2e
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-01-16 10:45:23 +0100

    Removed logging of interactions

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,6 @@
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
-    console.log(
-      `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction: ${interaction.commandName}`,
-    );
-
     if (!interaction.isCommand()) return;
 
     const command = interaction.client.commands.get(interaction.commandName);
