c5d3a62b6b1d4bfed13bde8d0ef1fcce87621852
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-02-13 10:06:48 +0100

    Added error handling for /game active

--- commands/game/gameActive.js
+++ commands/game/gameActive.js
@@ -1,26 +1,34 @@
 const Table = require('text-table');
 
 module.exports = async function execute(interaction) {
-  await interaction.client.gameService.getActiveGame().then((response) => {
-    if (response.data.id < 0) {
+  await interaction.client.gameService
+    .getActiveGame()
+    .then((response) => {
+      const tableData = [
+        ['Id', 'Game', 'Start'],
+        ['----', '----', '----'],
+        [response.data.id, response.data.name, response.data.startDate],
+      ];
+      const table = Table(tableData);
+      const content = `**Active game**\`\`\`${table}\`\`\``;
+
       interaction.reply({
-        content: 'No game is active currently.',
+        content,
         ephemeral: true,
       });
-      return;
-    }
-
-    const tableData = [
-      ['Id', 'Game', 'Start'],
-      ['----', '----', '----'],
-      [response.data.id, response.data.name, response.data.startDate],
-    ];
-    const table = Table(tableData);
-    const content = `**Active game**\`\`\`${table}\`\`\``;
-
-    interaction.reply({
-      content,
-      ephemeral: true,
+    })
+    .catch((error) => {
+      console.log(error.response.status);
+      if (error.response.status === 404) {
+        interaction.reply({
+          content: 'No game is active currently.',
+          ephemeral: true,
+        });
+      } else {
+        interaction.reply({
+          content: 'An error occured',
+          ephemeral: true,
+        });
+      }
     });
-  });
 };
