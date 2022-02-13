06fb9d296df6f18676678a429a2bc26a8165acf5
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-02-13 10:13:53 +0100

    Added error handling to /game add and list

--- commands/game/gameAdd.js
+++ commands/game/gameAdd.js
@@ -1,10 +1,18 @@
 module.exports = async function execute(interaction) {
   const name = interaction.options.getString('name');
 
-  await interaction.client.gameService.addGame({ name }).then((response) => {
-    interaction.reply({
-      content: `Game '${response.data.name}' (id ${response.data.id}) was added to the competition.`,
-      ephemeral: true,
+  await interaction.client.gameService
+    .addGame({ name })
+    .then((response) => {
+      interaction.reply({
+        content: `Game '${response.data.name}' (id ${response.data.id}) was added to the competition.`,
+        ephemeral: true,
+      });
+    })
+    .catch(() => {
+      interaction.reply({
+        content: 'An error occured',
+        ephemeral: true,
+      });
     });
-  });
 };
--- commands/game/gameList.js
+++ commands/game/gameList.js
@@ -1,18 +1,26 @@
 const Table = require('text-table');
 
 module.exports = async function execute(interaction) {
-  await interaction.client.gameService.getGames().then((response) => {
-    const tableData = [
-      ['Id', 'Game', 'Start'],
-      ['----', '----', '----'],
-      ...response.data.map((game) => [game.id, game.name, game.startDate || '']),
-    ];
-    const table = Table(tableData);
-    const content = `**Games**\`\`\`${table}\`\`\``;
+  await interaction.client.gameService
+    .getGames()
+    .then((response) => {
+      const tableData = [
+        ['Id', 'Game', 'Start'],
+        ['----', '----', '----'],
+        ...response.data.map((game) => [game.id, game.name, game.startDate || '']),
+      ];
+      const table = Table(tableData);
+      const content = `**Games**\`\`\`${table}\`\`\``;
 
-    interaction.reply({
-      content,
-      ephemeral: true,
+      interaction.reply({
+        content,
+        ephemeral: true,
+      });
+    })
+    .catch(() => {
+      interaction.reply({
+        content: 'An error occured',
+        ephemeral: true,
+      });
     });
-  });
 };
