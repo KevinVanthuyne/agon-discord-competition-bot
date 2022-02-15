ec0e284d54659c70eed18e4968f478c6416f642f
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-02-15 21:58:33 +0100

    Added a cron scheduled test message every 5 seconds in a configurable channel

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,19 @@
+const cron = require('cron');
+const { guildId, hallOfFameChannelId } = require('../config.json');
+
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+    // Schedule a message when the next game becomes active
+    const scheduledMessage = new cron.CronJob('*/5 * * * * *', () => {
+      const guild = client.guilds.cache.get(guildId);
+      const channel = guild.channels.cache.get(hallOfFameChannelId);
+      channel.send('Cron test');
+    });
+
+    scheduledMessage.start();
   },
 };
