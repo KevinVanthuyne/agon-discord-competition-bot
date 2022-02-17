97d9b8157b539d79c90b39a25678940b52d2f55c
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-02-17 20:36:10 +0100

    Post a message if the game's start date matches today

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const cron = require('cron');
+const { DateTime } = require('luxon');
 const { guildId, hallOfFameChannelId } = require('../config.json');
 
 module.exports = {
@@ -8,12 +9,28 @@ module.exports = {
     console.log(`Ready! Logged in as ${client.user.tag}`);
 
     // Schedule a message when the next game becomes active
-    // const scheduledMessage = new cron.CronJob('*/5 * * * * *', () => { // Every 5 seconds (for testing)
-    const scheduledMessage = new cron.CronJob('0 0 0 * * *', () => {
+    // Every 5 seconds (for testing)
+    const scheduledMessage = new cron.CronJob('*/5 * * * * *', () => {
       // Every midnight
-      const guild = client.guilds.cache.get(guildId);
-      const channel = guild.channels.cache.get(hallOfFameChannelId);
-      channel.send('Cron test');
+      // const scheduledMessage = new cron.CronJob('0 0 0 * * *', () => {
+      console.log('[Cron] Running the scheduled job.');
+      client.gameService
+        .getActiveGame()
+        .then((response) => response.data)
+        .then((game) => {
+          const gameStartDate = DateTime.fromISO(game.startDate);
+          if (!DateTime.now().hasSame(gameStartDate, 'day')) {
+            console.log('[Cron] Game start date did not match current date, skipping.');
+            return;
+          }
+          console.log('[Cron] Game start date matches today, posting new game.');
+          const guild = client.guilds.cache.get(guildId);
+          const channel = guild.channels.cache.get(hallOfFameChannelId);
+          channel.send(`Time for a new game: **${game.name}**!`);
+        })
+        .catch((e) => {
+          console.log('Something went wrong when executing the CronJob', e);
+        });
     });
 
     scheduledMessage.start();
