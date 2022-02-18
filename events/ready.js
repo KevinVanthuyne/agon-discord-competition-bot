721198f1b22c4724733338b9dca01b57b4af5a29
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-02-18 09:00:52 +0100

    Post game header image and name when announcing next game

--- events/ready.js
+++ events/ready.js
@@ -9,30 +9,39 @@ module.exports = {
     console.log(`Ready! Logged in as ${client.user.tag}`);
 
     // Schedule a message when the next game becomes active
+
     // Every 5 seconds (for testing)
     const scheduledMessage = new cron.CronJob('*/5 * * * * *', () => {
       // Every midnight
       // const scheduledMessage = new cron.CronJob('0 0 0 * * *', () => {
       console.log('[Cron] Running the scheduled job.');
-      client.gameService
-        .getActiveGame()
-        .then((response) => response.data)
-        .then((game) => {
-          const gameStartDate = DateTime.fromISO(game.startDate);
-          if (!DateTime.now().hasSame(gameStartDate, 'day')) {
-            console.log('[Cron] Game start date did not match current date, skipping.');
-            return;
-          }
-          console.log('[Cron] Game start date matches today, posting new game.');
-          const guild = client.guilds.cache.get(guildId);
-          const channel = guild.channels.cache.get(hallOfFameChannelId);
-          channel.send(`Time for a new game: **${game.name}**!`);
-        })
-        .catch((e) => {
-          console.log('Something went wrong when executing the CronJob', e);
-        });
+      announceNextGame(client);
     });
 
     scheduledMessage.start();
   },
 };
+
+function announceNextGame(client) {
+  client.gameService
+    .getActiveGame()
+    .then((response) => response.data)
+    .then((game) => {
+      const gameStartDate = DateTime.fromISO(game.startDate);
+      if (!DateTime.now().hasSame(gameStartDate, 'day')) {
+        console.log('[Cron] Game start date did not match current date, skipping.');
+        return;
+      }
+      console.log('[Cron] Game start date matches today, posting new game.');
+
+      const guild = client.guilds.cache.get(guildId);
+      const channel = guild.channels.cache.get(hallOfFameChannelId);
+      channel.send({
+        content: `Time for a new game! This month we are playing:\n\n> **${game.name}**\n\nGood luck, and have fun!`,
+        files: [{ attachment: game.gameStyle.headerImage }],
+      });
+    })
+    .catch((e) => {
+      console.log('Something went wrong when executing the CronJob', e);
+    });
+}
